fa90edf62cfe721338c4ea3007714b27037b7645
tigerhooduday <udaygarg6248@gmail.com>
2026-01-05 12:46:14 +0530

    commit

--- submit_job.py
+++ submit_job.py
@@ -1,6 +1,6 @@
 from orchestrator import run_scan
 
 if __name__ == "__main__":
-    result = run_scan("https://exceleratetechnologies.com/")
+    result = run_scan("https://juice-shop.herokuapp.com")
     print("\n=== FINAL RESULT ===")
     print(result)
